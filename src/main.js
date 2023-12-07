7469456bdb941a35a0785dde4a2151aaba2502ec
M10mo <mohdkanoongo@gmail.com>
2023-12-07 11:36:06 -0500

    build for deploy

---
+++ src/main.js
@@ -0,0 +1,4 @@
+//console.log("hello world");
+// document.querySelector("#app").innerHTML += '<h1>Hello world</h1>';
+//import palettes from './palettes.json'
+//console.log(palettes); // It's now regular JS code!
