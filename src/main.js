2ae960ac3d85ecdcb7d43e9be8f62ebe71cd277e
M10mo <mohdkanoongo@gmail.com>
2023-12-08 12:27:06 -0500

    JS initial changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,2 @@
-//console.log("hello world");
-// document.querySelector("#app").innerHTML += '<h1>Hello world</h1>';
-//import palettes from './palettes.json'
-//console.log(palettes); // It's now regular JS code!
+import palettes from "./palettes.json" assert {type: 'json'};
+console.log(palettes); // It's now regular JS code!
