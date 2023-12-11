523f987e5d5355397c99e50c7c1a174c45305942
M10mo <mohdkanoongo@gmail.com>
2023-12-11 17:34:38 -0500

    fetching from localStorage

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import palettes from "./palettes.json" assert {type: 'json'};
+import palettes from "./palettes.json" assert { type: "json" };
 console.log(palettes); // It's now regular JS code!
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,27 +7,38 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function addPalette() {
-  const colorOption1 = document.getElementById('colorOption1').value;
-  const colorOption2 = document.getElementById('colorOption2').value;
-  const colorOption3 = document.getElementById('colorOption3').value;
-  const temperature = document.getElementById('temperature').value;
+  const paletteTitle = document.getElementById('paletteTitle').value;
+  const color1 = document.getElementById('color1').value;
+  const color2 = document.getElementById('color2').value;
+  const color3 = document.getElementById('color3').value;
+  const temperature = document.querySelector('input[name="temperature"]:checked');
+
+  // Check if all required fields are filled
+  if (!paletteTitle || !color1 || !color2 || !color3 || !temperature) {
+    alert('Please fill in all fields.');
+    return;
+  }
 
   // Store user's information in localStorage
   const userPalette = {
-    colorOption1,
-    colorOption2,
-    colorOption3,
-    temperature
+    paletteTitle,
+    color1,
+    color2,
+    color3,
+    temperature: temperature.value
   };
 
   localStorage.setItem('userPalette', JSON.stringify(userPalette));
 
   // Display the new palette in the history
   displayPaletteHistory();
+
+  // Clear the form
+  document.getElementById('paletteForm').reset();
 }
 
 function displayPaletteHistory() {
-  const paletteHistoryDiv = document.getElementById('paletteHistory');
+  const paletteHistoryDiv = document.getElementById('app');
   paletteHistoryDiv.innerHTML = '';
 
   // Retrieve user's palette from localStorage
@@ -40,18 +51,25 @@ function displayPaletteHistory() {
     const paletteForm = document.createElement('div');
     paletteForm.className = 'colorPaletteBox';
 
-    for (const color of Object.values(userPalette)) {
+    const titleHeading = document.createElement('h4');
+    titleHeading.textContent = userPalette.paletteTitle;
+    paletteForm.appendChild(titleHeading);
+
+    const colorBoxes = document.createElement('div');
+    colorBoxes.className = 'colorPaletteBox';
+
+    for (const color of [userPalette.color1, userPalette.color2, userPalette.color3]) {
       const colorBox = document.createElement('div');
       colorBox.style.backgroundColor = color;
-      paletteForm.appendChild(colorBox);
+      colorBoxes.appendChild(colorBox);
     }
+
+    paletteForm.appendChild(colorBoxes);
+
+    const temperatureHeading = document.createElement('p');
+    temperatureHeading.textContent = `Temperature: ${userPalette.temperature}`;
+    paletteForm.appendChild(temperatureHeading);
 
     paletteHistoryDiv.appendChild(paletteForm);
   }
 }
-
-// Update temperature value display
-document.getElementById('temperature').addEventListener('input', () => {
-  const temperatureValue = document.getElementById('temperatureValue');
-  temperatureValue.textContent = document.getElementById('temperature').value;
-});
