dbca90693b80a8d3622875a8f7a47ff5f276a992
M10mo <mohdkanoongo@gmail.com>
2023-12-14 20:08:57 -0500

    changed to innerHTML

--- src/main.js
+++ src/main.js
@@ -1,78 +1,113 @@
 import palettes from "./palettes.json" assert { type: "json" };
 console.log(palettes); // It's now regular JS code!
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Retrieve saved palette from localStorage on page load
-  displayPaletteHistory();
-});
+// Store user's information in localStorage
 
-function addPalette() {
-  const paletteTitle = document.getElementById('paletteTitle').value;
-  const color1 = document.getElementById('color1').value;
-  const color2 = document.getElementById('color2').value;
-  const color3 = document.getElementById('color3').value;
-  const temperature = document.querySelector('input[name="temperature"]:checked');
 
-  // Check if all required fields are filled
-  if (!paletteTitle || !color1 || !color2 || !color3 || !temperature) {
-    alert('Please fill in all fields.');
-    return;
-  }
+function renderPalette() {
+  const paletteHistoryDiv = document.getElementById("app");
 
-  // Store user's information in localStorage
-  const userPalette = {
-    paletteTitle,
-    color1,
-    color2,
-    color3,
-    temperature: temperature.value
-  };
-
-  localStorage.setItem('userPalette', JSON.stringify(userPalette));
-
-  // Display the new palette in the history
-  displayPaletteHistory();
-
-  // Clear the form
-  document.getElementById('paletteForm').reset();
-}
-
-function displayPaletteHistory() {
-  const paletteHistoryDiv = document.getElementById('app');
-  paletteHistoryDiv.innerHTML = '';
-
+  paletteHistoryDiv.innerHTML = "";
   // Retrieve user's palette from localStorage
-  const storedPalette = localStorage.getItem('userPalette');
-
-
-
+  const storedPalette = localStorage.getItem("palette");
+const paletteDiv = document.querySelector('#palettes')
   if (storedPalette) {
-    const userPalette = JSON.parse(storedPalette);
-
-    // Create a new form with the stored palette
-    const paletteForm = document.createElement('div');
-    paletteForm.className = 'colorPaletteBox';
-
-    const titleHeading = document.createElement('h4');
-    titleHeading.textContent = userPalette.paletteTitle;
-    paletteForm.appendChild(titleHeading);
-
-    const colorBoxes = document.createElement('div');
-    colorBoxes.className = 'colorPaletteBox';
-
-    for (const color of [userPalette.color1, userPalette.color2, userPalette.color3]) {
-      const colorBox = document.createElement('div');
-      colorBox.style.backgroundColor = color;
-      colorBoxes.appendChild(colorBox);
-    }
-
-    paletteForm.appendChild(colorBoxes);
-
-    const temperatureHeading = document.createElement('p');
-    temperatureHeading.textContent = `Temperature: ${userPalette.temperature}`;
-    paletteForm.appendChild(temperatureHeading);
-
-    paletteHistoryDiv.appendChild(paletteForm);
+    storedPalette.foreach(palette => {
+  const div = document.createElement('div')
+      div.innerHTML = `
+        <h2>pallete title:${palette.title}<h2>
+        <p>color#1 : ${palette.color1}<p>
+        <p>color#2 : ${palette.color2}<p>
+        <p>color#3 : ${palette.color3}>
+        <p>Temperature: ${palette.temperature}<p>
+        `
+      paletteDiv.appendChild(div)
+      console.log(div)
+    })
+    
   }
+  
 }
-console.log(addPalette)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     const userPalette = JSON.parse(storedPalette);
+//     // Create a new form with the stored palette
+//     const paletteForm = document.createElement("div");
+//     paletteForm.className = "colorPaletteBox";
+//   }
+//   const titleHeading = document.createElement("h4");
+//   titleHeading.textContent = userPalette.paletteTitle;
+//   paletteForm.appendChild(titleHeading);
+
+//   const colorBoxes = document.createElement("div");
+//   colorBoxes.className = "colorPaletteBox";
+
+//   for (const color of [
+//     userPalette.color1,
+//     userPalette.color2,
+//     userPalette.color3,
+//   ]) {
+//     const colorBox = document.createElement("div");
+//     colorBox.style.backgroundColor = color;
+//     colorBoxes.appendChild(colorBox);
+//   }
+
+//   paletteForm.appendChild(colorBoxes);
+
+//   const temperatureHeading = document.createElement("p");
+//   temperatureHeading.textContent = `Temperature: ${userPalette.temperature}`;
+//   paletteForm.appendChild(temperatureHeading);
+
+//   paletteHistoryDiv.appendChild(paletteForm);
+// }
+
+
+
+// Add event listener for form submission
+
+document.getElementById("paletteForm").addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  const form = event.target;
+  const formData = new FormData(form);
+  const dataObj = Object.fromEntries(formData);
+  event.preventDefault();
+ 
+
+}
+
+
+
+
+  function submitPalettes() {
+    if (!localStorage.getItem('palette')) {
+     localStorage.setItem('palettes',JSON.stringify(palettes))
+   }
+    
+  }
+  
+
+ 
+
+const main = () => {
+  submitPalettes();
+  renderPalette()
+
+}
+main()
